e48ce6eaffbc95185a895e30dc99c6162e916e50
faiz <wizfaiz@icloud.com>
2024-07-04 07:37:44 -0700

    updates

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from flask import Flask, render_template, request, redirect
+from flask_pymongo import PyMongo
+
+app = Flask(__name__, template_folder='../frontend/templates')
+# mongo = PyMongo(app=app)
+
+@app.route('/', methods=['GET'])
+def index():
+    """index file"""
+    return render_template('index.html', title='Home')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
