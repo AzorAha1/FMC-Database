fe5d03a5883084434cddcdb19a1ea9815e957154
aliyu <aliyuabubakar11117@gmail.com>
2024-07-06 15:13:18 +0100

    dashboard file for admin

--- frontend/statics/animate.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('container');
-    // Add the 'show' class to start the animation
-    setTimeout(() => {
-        container.classList.add('show');
-    }, 100); // Delay to ensure the animation runs
-});
